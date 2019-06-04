c1ab273dfddfcc3aac596e9c48c2856dcaaff147
Emile Sonneveld <contact@emilesonneveld.be>
2019-06-04 13:08:36 +0200

    And back to windows.

--- SccClient/src/be/scc/client/WindowsClassicLookAndFeelExt.java
+++ SccClient/src/be/scc/client/WindowsClassicLookAndFeelExt.java
@@ -2,12 +2,12 @@ package be.scc.client;
 
 import javax.swing.UIDefaults;
 
-public class WindowsClassicLookAndFeelExt
-        //extends com.sun.java.swing.plaf.windows.WindowsLookAndFeel
-        //extends com.sun.java.swing.plaf.motif.MotifLookAndFeel
 /**
  * https://stackoverflow.com/a/3587482/1448736
  */
+public class WindowsClassicLookAndFeelExt
+        //extends com.sun.java.swing.plaf.windows.WindowsLookAndFeel
+        //extends com.sun.java.swing.plaf.motif.MotifLookAndFeel
         extends javax.swing.plaf.metal.MetalLookAndFeel
 {
     @Override
